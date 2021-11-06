b322aa6fbf66f2943103fb891ef6c9378da481b0
mahak <makhariamahak@gmail.com>
2021-11-06 14:32:33 +0530

    add: css for generator section

--- src/components/slogan-generator/SloganGeneratorSection.js
+++ src/components/slogan-generator/SloganGeneratorSection.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import GeneratedSlogan from './GeneratedSlogan';
+import styles from './slogan.module.css'
 
 const SloganGeneratorSection = () => {
     return (
-        <div >
-            <h4>Free slogan maker</h4>
-            <p>Simply enter a term that describes your business, and get up to 1,000 relevant slogans for free.</p>
-            <p>Word for your slogan</p>
-            <input type="text" />
-            <button>Generate slogans</button>
-            <GeneratedSlogan />
+        <div className={styles.generatorCard}>
+            <h4 className={styles.heading}>Free slogan maker</h4>
+            <p className={styles.desc}>Simply enter a term that describes your business, and get up to 1,000 relevant slogans for free.</p>
+            <p className={styles.inputlabel}>Word for your slogan</p>
+            <div className={styles.inputBox}>
+                <input type="text" />
+            </div>
+            <button className={styles.blueButton}>Generate slogans</button>
+            {/* <GeneratedSlogan /> */}
 
         </div>
     );
