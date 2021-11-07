37cba1fd4da57b64b696a25533c74360872aa8b2
mahak <makhariamahak@gmail.com>
2021-11-07 11:52:33 +0530

    add: js for copy/copied toasts

--- src/components/slogan-generator/SloganCard.js
+++ src/components/slogan-generator/SloganCard.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './slogan.module.css'
 
-const SloganCard = ({ slogan, copied = true }) => {
+const SloganCard = ({ slogan }) => {
+
+    const [copy, setCopy] = useState(false);
+    const [copied, setCopied] = useState(false);
+
     return (
         <>
-            <div className={styles.sloganBox}>
+            <div className={styles.sloganBox} onMouseOver={() => { setCopy(true) }}
+                onMouseOut={() => { setCopy(false); setCopied(false) }}
+                onClick={() => setCopied(true)}
+            >
                 <div className={styles.sloganText}>
                     {slogan}
                 </div>
-                {copied ? <div className={styles.copiedToast} >Copied!</div>
-                    : <div className={styles.copyToast} >Click to copy</div>}
+
+                {copy && copied === false ? <div className={styles.copyToast} >Click to copy</div>
+                    : null}
+
+                {copied ? <div className={styles.copiedToast} >Copied!</div> : null}
 
             </div>
 
